01dd938df5f73bbad560123d4cb5375784d86dae
RobHam99 <robert.hamilton.2017@uni.strath.ac.uk>
2020-12-05 17:35:28 +0000

    Added rectangle resizer

---
+++ test.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+
+def makerect(p1, p2):
+    xdif = p2[0]-p1[0]
+    ydif = p2[1]-p1[1]
+
+    if xdif == 1.5*ydif: # if length is 1.5 height return
+        return p1, p2
+
+    elif xdif < 1.5*ydif: # if length is less than 1.5 height
+        realfac = 1.5*ydif # what length should be
+        deltax = realfac-xdif # changed to x to get 1.5 heigh
+
+        # if change will stay in screen coords
+        if (p1[0] - (deltax/2)) >= 0 and (p2[0] + (deltax/2)) <= w:
+            newx1 = int(p1[0] - (deltax/2))
+            newx2 = int(p2[0] + (deltax/2))
+            return (newx1, p1[1]), (newx2, p2[1])
+
+        # if change will go out of screen left
+        elif (p1[0] - (deltax/2)) < 0 and (p2[0] + (deltax/2)) <= w:
+            for i in range(w):
+                if (p1[0] - (deltax/2) + i) >= 0:
+                    newx1 = int((p1[0] - (deltax/2) + i))
+                    deltax1 = p1[0] - newx1
+                    newx2 = int((p2[0] + (deltax - deltax1)))
+                    return (newx1, p1[1]), (newx2, p2[1])
+
+        # if change will go out of screen right
+        elif (p1[0] - (deltax/2)) >= 0 and (p2[0] + (deltax/2)) > w:
+            for i in range(w):
+                if (p2[0] + (deltax/2) - i) <= w:
+                    newx2 = int((p2[0] + (deltax/2) - i))
+                    deltax2 = newx2 - p2[0]
+                    newx1 = int(p1[0] - (deltax - deltax2))
+                    return (newx1, p1[1]), (newx2, p2[1])
+
+    else: # if length is more than 1.5 height
+        realfac = xdif/1.5 # what height should be
+        deltay = realfac-ydif
+
+        # if change will stay in screen coords
+        if (p1[1] - (deltay/2)) >= 0 and (p2[1] + (deltay/2)) <= h:
+            newy1 = int(p1[1] - (deltay/2))
+            newy2 = int(p2[1] + (deltay/2))
+            return (p1[0], newy1), (p2[0], newy2)
+
+        # if change will go out of screen top
+        elif (p1[1] - (deltay/2)) < 0 and (p2[1] + (deltay/2)) <= h:
+            for i in range(h):
+                if (p1[1] - (deltay/2) + i) >= 0:
+                    newy1 = int((p1[1] - (deltay/2) + i))
+                    deltay1 = p1[1] - newy1
+                    newy2 = int((p2[1] + (deltay - deltay1)))
+                    return (p1[0], newy1), (p2[0], newy2)
+
+        # if change will go out of screen bottom
+        elif (p1[1] - (deltay/2)) >= 0 and (p2[1] + (deltay/2)) > h:
+            for i in range(h):
+                if (p2[1] + (deltay/2) - i) <= h:
+                    newy2 = int((p2[1] + (deltay/2) - i))
+                    deltay2 = newy2 - p2[1]
+                    newy1 = int(p1[1] - (deltay - deltay2))
+                    return (p1[0], newy1), (p2[0], newy2)
+
+
+
+
+w = 600
+h = 400
+p1 = (100, 120)
+p2 = (600, 140)
+
+p3, p4 = makerect(p1, p2)
+
+print(p3)
+print(p4)
+print((p4[0]-p3[0])/(p4[1]-p3[1]))
--- toast.py
+++ toast.py
@@ -1,12 +1,84 @@
 import pygame
 import numpy as np
 import colorsys
-from numba import jit
+#from numba import jit
+
+
+def makerect(p1, p2):
+    """
+    Function to make sure the zoom doesn't squish dimensions
+    e.g. if you draw a rectangle way longer than it is high
+    it will rearrange it into a rectangle where length = 1.5*height
+    """
+
+    xdif = p2[0]-p1[0]
+    ydif = p2[1]-p1[1]
+
+    if xdif == 1.5*ydif: # if length is 1.5 height return
+        return p1, p2
+
+    elif xdif < 1.5*ydif: # if length is less than 1.5 height
+        realfac = 1.5*ydif # what length should be
+        deltax = realfac-xdif # changed to x to get 1.5 heigh
+
+        # if change will stay in screen coords
+        if (p1[0] - (deltax/2)) >= 0 and (p2[0] + (deltax/2)) <= w:
+            newx1 = int(p1[0] - (deltax/2))
+            newx2 = int(p2[0] + (deltax/2))
+            return (newx1, p1[1]), (newx2, p2[1])
+
+        # if change will go out of screen left
+        elif (p1[0] - (deltax/2)) < 0 and (p2[0] + (deltax/2)) <= w:
+            for i in range(w):
+                if (p1[0] - (deltax/2) + i) >= 0:
+                    newx1 = int((p1[0] - (deltax/2) + i))
+                    deltax1 = p1[0] - newx1
+                    newx2 = int((p2[0] + (deltax - deltax1)))
+                    return (newx1, p1[1]), (newx2, p2[1])
+
+        # if change will go out of screen right
+        elif (p1[0] - (deltax/2)) >= 0 and (p2[0] + (deltax/2)) > w:
+            for i in range(w):
+                if (p2[0] + (deltax/2) - i) <= w:
+                    newx2 = int((p2[0] + (deltax/2) - i))
+                    deltax2 = newx2 - p2[0]
+                    newx1 = int(p1[0] - (deltax - deltax2))
+                    return (newx1, p1[1]), (newx2, p2[1])
+
+    else: # if length is more than 1.5 height
+        realfac = xdif/1.5 # what height should be
+        deltay = realfac-ydif
+
+        # if change will stay in screen coords
+        if (p1[1] - (deltay/2)) >= 0 and (p2[1] + (deltay/2)) <= h:
+            newy1 = int(p1[1] - (deltay/2))
+            newy2 = int(p2[1] + (deltay/2))
+            return (p1[0], newy1), (p2[0], newy2)
+
+        # if change will go out of screen top
+        elif (p1[1] - (deltay/2)) < 0 and (p2[1] + (deltay/2)) <= h:
+            for i in range(h):
+                if (p1[1] - (deltay/2) + i) >= 0:
+                    newy1 = int((p1[1] - (deltay/2) + i))
+                    deltay1 = p1[1] - newy1
+                    newy2 = int((p2[1] + (deltay - deltay1)))
+                    return (p1[0], newy1), (p2[0], newy2)
+
+        # if change will go out of screen bottom
+        elif (p1[1] - (deltay/2)) >= 0 and (p2[1] + (deltay/2)) > h:
+            for i in range(h):
+                if (p2[1] + (deltay/2) - i) <= h:
+                    newy2 = int((p2[1] + (deltay/2) - i))
+                    deltay2 = newy2 - p2[1]
+                    newy1 = int(p1[1] - (deltay - deltay2))
+                    return (p1[0], newy1), (p2[0], newy2)
+
+
 
 #class mandelbrot:
     #def __init__(self, max_iterations):
         #self.max_iterations = max_iterations
-@jit(nopython=True)
+#@jit(nopython=True)
 def calculate(x):
     z = 0
     n = 0
@@ -59,8 +131,7 @@ screen.fill((255, 255, 255))
 pygame.display.set_caption('The Mandelbrot Set')
 
 complexMatrix = np.zeros((w, h), dtype=complex)
-max_iterations = 1000
-
+max_iterations = 80
 reStart = -2
 reEnd = 1
 imStart = -1
@@ -82,7 +153,8 @@ while running:
             rectStart = event.pos
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             rectEnd = event.pos
-            zoom(rectStart, rectEnd)
+            rectS, rectE = makerect(rectStart, rectEnd)
+            zoom(rectS, rectE)
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
                 plot(reStart, reEnd, imStart, imEnd)
